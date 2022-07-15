dde2e29cd0d284e9721787eb96f4bcf7c2c32502
weston-bailey <wdbailey87@gmail.com>
2022-07-15 10:21:48 -0700

    banishes binaries and debug files, adds input

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define LSH_RL_BUFSIZE 1024
+
 void lsh_loop(void);
+char *lsh_read_line(void);
 
 int main (int argc, char *argv[]) {
 	// Load config files, if any.
@@ -15,17 +18,58 @@ int main (int argc, char *argv[]) {
 void lsh_loop(void) 
 {
 	char *line;
-	char **args;
-	int status;
+	/* char **args; */
+	int status = 0;
 
 	do {
 		printf("> ");
-		/* line = lsh_read(); */
-		/* args = lsh_split_lint(line); */
+		line = lsh_read_line();
+		/* args = lsh_split_line(line); */
 		/* status = lsh_exectute(args); */
 
-		/* free(line); */
+		free(line);
 		/* free(args); */
 
 	} while (status);
+}
+
+char *lsh_read_line(void)
+{
+	int bufsize = LSH_RL_BUFSIZE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufsize);
+	int c = 0;
+
+	if (!buffer) {
+		// print to file
+		fprintf(stderr, "lsh: allocation error\n:");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1) {
+		// read a character
+		c = getchar();
+		// if we hit EOF, replace it with null character and return
+		if (c == EOF || c == '\n') {
+			buffer[position] = '\0';
+			return buffer;
+		} else {
+			buffer[position] = c;
+			printf("the buffer is %s\n", buffer);
+		}
+
+		position++;
+
+		// if we have exceeded the buffer, reallocate.
+		if (position >= bufsize) {
+			bufsize += LSH_RL_BUFSIZE;
+			buffer = realloc(buffer, bufsize);
+			
+			if (!buffer) {
+				// print to file
+				fprintf(stderr, "lsh: allocation error\n:");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
 }
