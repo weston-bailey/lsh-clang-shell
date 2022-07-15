9cc78abcdfbc20b8519f960e37c0ff1a9818b85e
weston-bailey <wdbailey87@gmail.com>
2022-07-15 13:06:38 -0700

    adds execute

--- main.c
+++ main.c
@@ -11,6 +11,27 @@ void lsh_loop(void);
 char *lsh_read_line(void);
 char **lsh_split_line(char *line);
 int lsh_launch(char **args);
+int lsh_execute(char **args);
+
+// builtins 
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+int lsh_num_builtins(void);
+
+// builtin commands and their corresponding functions
+char *builtin_str[] = {
+	"cd",
+	"help",
+	"exit"
+};
+
+// array of function pointers that take an array for strings and return an int
+int (*builtin_func[]) (char **) = {
+	&lsh_cd,
+	&lsh_help,
+	&lsh_exit
+};
 
 int main (int argc, char *argv[]) {
 	// Load config files, if any.
@@ -31,7 +52,7 @@ void lsh_loop(void)
 		printf("> ");
 		line = lsh_read_line();
 		args = lsh_split_line(line);
-		/* status = lsh_exectute(args); */
+		status = lsh_execute(args);
 
 		free(line);
 		free(args);
@@ -61,7 +82,7 @@ char *lsh_read_line(void)
 			return buffer;
 		} else {
 			buffer[position] = c;
-			printf("the buffer is %s\n", buffer);
+			/* printf("the buffer is %s\n", buffer); */
 		}
 
 		position++;
@@ -139,4 +160,56 @@ int lsh_launch(char **args)
 	}
 	
 	return 1;		
+}
+
+int lsh_num_builtins(void) 
+{
+	return sizeof(builtin_str) / sizeof(char *);
+}
+
+int lsh_cd(char **args) {
+	if (args[1] == NULL) {
+		fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+	} else {
+		if (chdir(args[1]) != 0) {
+			perror("lsh");
+		}
+	}
+
+	return 1;
+}
+
+int lsh_help(char **args)
+{
+  int i;
+  printf("lsh shell\n");
+
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int lsh_exit(char **args) 
+{
+	return 0;
+}
+
+int lsh_execute(char **args)
+{
+	int i = 0;
+	// an empty command was entered
+	if (args[0] == NULL) {
+		return 1;
+	}
+
+	for (i = 0; i < lsh_num_builtins(); i++) {
+		if (strcmp(args[0], builtin_str[i]) == 0) {
+			return (*builtin_func[i])(args);
+		}
+	}
+
+	return lsh_launch(args);
 }
